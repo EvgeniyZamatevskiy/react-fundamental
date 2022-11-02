a17ec7c8d4149c7132c0db744e56bb77a15e32fd
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-11-02 17:04:58 +0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState, FormEvent } from "react"
+import "./App.css"
+import { UseState } from "hooks/useState"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type ItemsType = {
+  id: number
+  text: string
 }
 
-export default App;
+
+export const App: FC = () => {
+
+  const [items, setItems] = useState<ItemsType[]>([])
+  const [text, setText] = useState("")
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const item = {text, id: Date.now()}
+    setItems([...items, item])
+  }
+
+  return (
+    <div>
+      {/*<UseState/>*/}
+      <h3>Список дел</h3>
+      <TodoList items={items}/>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={(event) => setText(event.currentTarget.value)}/>
+        <button>Добавить № {items.length + 1}</button>
+      </form>
+    </div>
+  )
+}
+
+type TodoListPropsType = {
+  items: ItemsType[]
+}
+
+export const TodoList: FC<TodoListPropsType> = ({items}) => {
+
+
+  return (
+    <div>
+      <ul>
+        {items.map((item) => {
+          return <li key={item.id}>{item.text}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
